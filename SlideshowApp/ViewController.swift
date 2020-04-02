d1f12588ea8e05a1d0b1faa4570c0e84a2094f96
momota-fukuda <momota-fukuda@united.jp>
2020-04-02 17:19:30 +0900

    とりあえずスライドショーするように

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,11 +10,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imagePathBase = "image"
+    var nowIndex = 0
+    let imageNum = 5
+    
+    var images: [UIImage] = []
+    
+    var timer :Timer? = nil
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for index in 0..<self.imageNum {
+            let image = UIImage(named: self.createImagePath(index: index))
+            if image == nil {
+                continue;
+            }
+            
+            self.images.append(image!)
+        }
+        
+        self.nowIndex = self.images.startIndex
+        self.updateImage(index: self.nowIndex)
     }
 
-
+    func createImagePath(index :Int) -> String {
+        return "\(self.imagePathBase)\(index)"
+    }
+    
+    func changeNextImage() {
+        self.nowIndex += 1
+        if self.nowIndex >= self.images.endIndex {
+            self.nowIndex = self.images.startIndex
+        }
+        
+        self.updateImage(index: self.nowIndex)
+    }
+    
+    func changeBeforeImage() {
+        self.nowIndex -= 1
+        if self.nowIndex < self.images.startIndex {
+            self.nowIndex = self.images.endIndex - 1
+        }
+        
+        self.updateImage(index: self.nowIndex)
+    }
+    
+    func updateImage(index :Int) {
+        if (self.nowIndex < self.images.startIndex) || (self.images.endIndex <= self.nowIndex) {
+            return
+        }
+        
+        self.imageView.image = self.images[self.nowIndex]
+    }
+    
+    @objc func onUpdateTimer(timer :Timer){
+        self.changeNextImage()
+    }
+    
+    @IBAction func onTapNextButton(_ sender: UIButton) {
+        self.changeNextImage()
+    }
+    @IBAction func onTapBackButton(_ sender: UIButton) {
+        self.changeBeforeImage()
+    }
+    @IBAction func onTapTogglePlayButton(_ sender: UIButton) {
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(self.onUpdateTimer(timer:)), userInfo: nil, repeats: true)
+            return
+        }
+        
+        self.timer?.invalidate()
+        self.timer = nil
+    }
 }
 
